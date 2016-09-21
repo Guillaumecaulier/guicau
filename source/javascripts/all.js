40215257ed07f43e07cf4881dbd62bb4fe9f920e
Guillaume Caulier <cauliergui@gmail.com>
2016-09-21 23:58:26 +0200

    new home page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,29 +1,54 @@
 //= require TweenMax.min
 //= require TweenLite.min
-//= require DrawSVGPlugin.min
 //= require TimelineMax.min
-//= require AttrPlugin.min
-//= require MorphSVGPlugin.min
 //= require SplitText.min
+//= require DrawSVGPlugin.min
+//= require ScrollToPlugin.min
 //= require parallax.min
+
+
+
 
 function random(min, max){
   return (Math.random() * (max - min)) + min;
 }
 
 $(document).ready(function() {
-  var ico = $(".fa-angle-down");
   var nav = $(".main-nav");
+  var header = $(".loader");
+  var headercont = $("#loader");
+  var presentation = $("#presentation")
   var tl = new TimelineMax();
   tl.staggerFrom(".drawme", 2, {drawSVG:0}, 0.1)
-  .from(".black", 3, {autoAlpha:0, scale: .1, yoyo: true})
-  .to(ico, 0.5, {y:20, autoAlpha:1, repeat:1})
-  .from(nav, 1, {x:-200});
+    .from(".black", 1, {autoAlpha:0, scale: .1, yoyo: true})
+    .to(header, 3, { y:-1500, ease: Power2.easeOut}, "+=0.5")
+    .to(headercont, 1, {scale: 0.5}, "-=3")
+    .from(nav, 1, {x:-200}, "-=2")
+    .from(presentation, 2, {scale: 0}, "-=2")
+
+
+
+
+  var mouseX, mouseY;
+  var ww = $( window ).width();
+  var wh = $( window ).height();
+  var traX, traY;
+  $(document).mousemove(function(e){
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = ((4 * mouseX) / 570) + 80;
+    traY = ((4 * mouseY) / 570) + 40;
+    console.log(traX);
+    $(".background-move").css({"background-position": traX + "%" + traY + "%"});
+  });
+
+  $(".gopro").click(function(){
+  TweenMax.to(window, 1,{scrollTo: "#projects"})
+})
+
+
+
 });
-
-
-
-
 
 
 
@@ -43,15 +68,9 @@ $(split.chars).each(function(i){
     scale: .1,
     delay: i * .02,
     yoyo: true,
-  });
+  })
 });
 
-
-var backblue = $(".background-blue")
-
-TweenLite.from(backblue, 2, {
-  x:300, autoAlpha: 0
-});
 
 
 
